97c0241468eb92b180647ecb6fdcd430623d826f
Darren <DarrenLeel33@gmail.com>
2021-05-17 15:51:41 -0400

    commit tree 0af151ef164dd24f6204470695038881f9a742df

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Darren Lee'
+
+const welcomeMessage = 'Welcome to Flatbook, Darren Lee!'
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, DARREN LEE!"
+
+const shortGreeting = "Welcome, D!"
